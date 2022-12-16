3ac7b74cae2acb07e965bd25254e9dd4c6b3a828
Daithí <davecoll3@gmail.com>
2022-12-16 12:58:23 +0000

    feat: js game logic added and js used to add/subtract from game score/time

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-//Game instructions modal overlay
+// Game instructions modal overlay
 $(document).ready(function(){
 
   $('#instructions').click(function(){
@@ -10,24 +10,74 @@ $(document).ready(function(){
 
 });
 
-//Game selectors
+// Game selectors
 const score = document.querySelector('.score span');
-const time = document.querySelector('time span');
-const blocks = document.querySelectorAll('.blocks');
+const time = document.querySelector('.time span');
+const blocks = document.querySelectorAll(".block");
 const startButton = document.querySelector('.buttons #start');
 const resetButton = document.querySelector('.buttons #reset');
 
-//Initial score
+// Initial score
 let points = 0;
 
-//Hide start button on click and display reset button
+// Hide start button on click and display reset button
 startButton.addEventListener('click', () => {
   startButton.style.display = 'none';
   resetButton.style.display = 'inline-block';
+
+  // Initial game time
+  let remainingTime = 10;
+
+  // Update time and score on the page
+  time.innerHTML = remainingTime;
+  score.innerHTML = points;
+
+  // Start game
+  const startGame = setInterval(() => {
+    let arrayNumber = Math.floor(Math.random() * 12);
+    let currentBlock = blocks[arrayNumber];
+
+    // Change blocks div text to bug icon
+    currentBlock.innerHTML = `<p><i class="fa-solid fa-bug"></i></p>`;
+
+    // Add points when user clicks on div containing bug icon
+    for(let i = 0; i < blocks.length; i++) {
+      blocks[i].addEventListener('click', () => {
+        if (blocks[i].innerHTML.includes('<i class="fa-solid fa-bug"></i>')) {
+          points++;
+          score.innerHTML = points;
+          blocks[i].innerHTML = '&lt;/&gt;';
+        }
+      });
+    }
+
+    // After 2 seconds remove bug icon
+    setTimeout(() => {
+      currentBlock.innerHTML = `<p>&lt;/&gt;</p>`;
+    }, 2000);
+  }, 3000);
+
+  // Decrement game time every second
+  const timeInterval = setInterval(() => {
+    remainingTime--;
+    time.innerHTML = remainingTime;
+    if (remainingTime === 0) {
+      alert(`GAME OVER! Your score is ${points}`);
+
+      clearInterval(startGame);
+      clearInterval(timeInterval);
+    }
+  }, 1000);
 
   //Hide reset button on click and display start button
   resetButton.addEventListener('click', () => {
     resetButton.style.display = 'none';
     startButton.style.display = 'inline-block';
+
+    // Reset time and score
+    points = 0;
+    remainingTime = 10;
+    time.innerHTML = remainingTime;
+    score.innerHTML = points;
   });
 });
